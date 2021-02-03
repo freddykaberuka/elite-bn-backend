c69efb6e407c00c5aae8327e5105ba6dd82356ad
ISHIMWE Bernard <isbernard2001@gmail.com>
2021-02-03 13:40:15 +0200

    Final commit

--- src/app.js
+++ src/app.js
@@ -4,18 +4,16 @@ import dotenv from 'dotenv';
 import passport from 'passport';
 import router from './routes/index';
 import swaggerDocs from './docs/index';
-import swaggerJsDoc from 'swagger-jsdoc';
 
 dotenv.config();
 
-const swaggerJSDocFormation = swaggerJsDoc(swaggerDocs);
 const app = express();
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
-router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJSDocFormation));
+router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(router);
 
 export default app;
