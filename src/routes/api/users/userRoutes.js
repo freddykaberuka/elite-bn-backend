40f38c74372c6dadc1fef7118629f8535502102a
NDAYISHIMIYE Bertin <neddybertin@gmail.com>
2021-02-03 12:19:24 -0800

    arranging user routes

--- src/routes/api/users/userRoutes.js
+++ src/routes/api/users/userRoutes.js
@@ -8,14 +8,15 @@ import { UserAuthentication } from '../../../middlewares/auth';
 import {upload} from '../../../helpers/multer';
 
 const router = express.Router();
-router.post('/signup', validateUserData.createUser, validateUserData.verifyIfEmailisAvailable ,UserController.signUp);
-router.post('/signin', validateUserData.validateUserSigninData, validateUserData.verifySignin, UserController.signIn)
-router.get('/verifyEmail/:token', validateUserData.verificationValidation ,UserController.accountVerification);
-router.get('/auth/google/callback', passport.authenticate('google'), Social.Oauth);
-router.get('/oauth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
-router.get('/oauth/facebook/', passport.authenticate('facebook', {scope: ['public_profile', 'email']}));
-router.get('/facebook/callback', passport.authenticate('facebook'), Social.Oauth);
-router.patch('/updateProfile',upload.single('profilePicture'), UserAuthentication, UserController.updateProfile);
-router.get('/profile/:id', UserAuthentication, UserController.getProfile);
+router
+ .post('/signup', validateUserData.createUser, validateUserData.verifyIfEmailisAvailable ,UserController.signUp)
+ .post('/signin', validateUserData.validateUserSigninData, validateUserData.verifySignin, UserController.signIn)
+ .get('/verifyEmail/:token', validateUserData.verificationValidation ,UserController.accountVerification)
+ .get('/auth/google/callback', passport.authenticate('google'), Social.Oauth)
+ .get('/oauth/google', passport.authenticate('google', {scope: ['profile', 'email']}))
+ .get('/oauth/facebook/', passport.authenticate('facebook', {scope: ['public_profile', 'email']}))
+ .get('/facebook/callback', passport.authenticate('facebook'), Social.Oauth)
+ .get('/profile/:id', UserAuthentication, UserController.getProfile)
+ .patch('/updateProfile',upload.single('profilePicture'), UserAuthentication, UserController.updateProfile);
 export default router;
 
