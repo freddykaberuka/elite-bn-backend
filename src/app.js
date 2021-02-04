27c55ddd2e98d5b5e8fc11637f03d1ae052c24f2
ISHIMWE Bernard <isbernard2001@gmail.com>
2021-02-04 15:15:57 +0200

    Rebasing 2

--- src/app.js
+++ src/app.js
@@ -4,16 +4,18 @@ import dotenv from 'dotenv';
 import passport from 'passport';
 import router from './routes/index';
 import swaggerDocs from './docs/index';
+import swaggerJsDoc from 'swagger-jsdoc';
 
 dotenv.config();
 
+const swaggerJSDocFormation = swaggerJsDoc(swaggerDocs);
 const app = express();
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
-router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJSDocFormation));
 app.use(router);
 
 export default app;
