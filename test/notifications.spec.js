d88b52ce44c119f13d04be5ae9269e2c3a126175
NDAYISHIMIYE Bertin <neddybertin@gmail.com>
2021-02-18 10:25:05 -0800

    finish testing

--- test/notifications.spec.js
+++ test/notifications.spec.js
@@ -1,5 +1,6 @@
+import mocha from 'mocha';
 import chaiHttp from 'chai-http';
-import chai, { expect } from 'chai';
+import chai from 'chai';
 import app from '../src/index';
 
 require('@babel/polyfill');
@@ -11,11 +12,18 @@ const manager = {
   email: 'trojanx07@gmail.com',
   password: 'admin1234'
 };
+const user = {
+  email: 'neddyberry@gmail.com',
+  password: 'admin1234'
+};
 
 let token = '';
-let tokenId = '';
+let Usertoken = '';
+let userId= '';
+let managerId='';
+let notificationId= '';
 const permissionName = 'assignUsertoManager';
-
+const { it, describe } = mocha;
 describe('User Assign tests', () => {
   it('login manager', (done) => {
     chai.request(app)
@@ -24,18 +32,29 @@ describe('User Assign tests', () => {
       .end((err, response) => {
         response.should.have.status(200);
         token = response.body.data.token;
-        tokenId = response.body.data.userInfo.id;
+        managerId= response.body.data.userInfo.id;
         done();
       });
   });
+  it('login a user ', (done) => {
+    chai.request(app)
+      .post('/api/v1/users/signin')
+      .send(user)
+      .end((err, response) => {
+        response.should.have.status(200);
+        Usertoken = response.body.data.token;
+        userId= response.body.data.userInfo.id;
+        done();
+      });
+  });
+
   it('It should allow Manager to assign user to a Manager', (done) => {
     chai.request(app)
       .put('/api/v1/users/assign/manager')
       .set('Authorization', token)
       .set('permission_name', permissionName)
-      .send({ id: 3, lineManagerId: 10 })
+      .send({ id: userId, lineManagerId: managerId })
       .end((err, response) => {
-        eventEmitter.emit('userAssignedToManager', { lineManagerId: 10, id: 3, tokenId});
         response.should.have.status(200);
         done();
       });
@@ -43,11 +62,25 @@ describe('User Assign tests', () => {
   it('It should retrieve notifications for the signed in user', (done) => {
     chai.request(app)
       .get('/api/v1/notifications')
-      .set('Authorization', token)
+      .set('Authorization', Usertoken)
       .end((err, response) => {
-          console.log('%%%%%%%%%%%%%%%%%%%%%%%', response.body)
         response.should.have.status(200);
         response.body.should.be.a('object');
+        response.body.should.have.property('message');
+        response.body.should.have.property('data');
+        notificationId = response.body.data[0].id;
+        done();
+      });
+  });
+  it('It should retrieve one notification by id for the signed in user', (done) => {
+    chai.request(app)
+      .get(`/api/v1/notifications/${notificationId}`)
+      .set('Authorization', Usertoken)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.body.should.be.a('object');
+        response.body.should.have.property('message');
+        response.body.should.have.property('data');
         done();
       });
   });
