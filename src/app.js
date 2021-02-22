3a8afc97345ca92d6a17b96536b1c342f1578e7c
nkalyesubula <49583916+nkalyesubula@users.noreply.github.com>
2021-02-22 15:42:21 +0300

    Update app.js
    Remove duplicates

--- src/app.js
+++ src/app.js
@@ -12,8 +12,6 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
