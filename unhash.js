c2963229c783815f90e352abbeaac435cb4ef1b0
“ishimwebernard” <isbernard2001@gmail.com>
2021-02-23 10:20:31 +0200

    mend

--- unhash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-const hashedPassword = '$2b$10$5bVBlFQxpo3.laIrak8wFuRk5RkfkmLd5N1EuHVTlLFUto/eEoGqe';
-
-(async()=>{
-    const comp = await bcrypt.hash('admin1234', 10,(err, res)=>{
-        console.log(res);
-    })
-
-})();
